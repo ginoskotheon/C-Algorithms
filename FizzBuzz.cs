361934c8345e9ed14dac17c6699c79a2c8d9448c
William McDonald <wmcdonald@StevesDoors.com>
2017-08-11 10:13:30 -0500

    Fizz Buzz

---
+++ FizzBuzz.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace C_Algorithms
+{
+    class FizzBuzz
+    {
+        static void Main(string[] args)
+        {
+            Fizzbuzz(15);   
+        }
+
+    
+
+        static void Fizzbuzz(int num)
+        {
+            // string ans = "";
+            for (int i = 0; i < num; i++)
+            {
+                if(i % 5 == 0 && i % 3 == 0){
+                    Console.WriteLine("Fizzbuzz");
+                }
+                else if (i % 5 == 0)
+                {
+                     Console.WriteLine("Fizz");
+                }
+                else if (i % 3 == 0)
+                {
+                     Console.WriteLine("Buzz");
+                }
+                else
+                {
+                     Console.WriteLine(i.ToString());
+                }
+            }    
+        }  
+    }
+}
